a75c2b7ba3f0b8732149b8fd4af0781fba2cc0c7
Lizeth Del Rio <liz-delrio@outlook.com>
2023-04-28 03:17:31 -0500

    Se crea y se finaliza el footer

--- src/app/ui/pages/objective/objective.component.ts
+++ src/app/ui/pages/objective/objective.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { InformationComponent } from '../../components/information/information.component';
 import { CardsComponent } from '../../components/cards/cards.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-objective',
@@ -11,7 +12,8 @@ import { CardsComponent } from '../../components/cards/cards.component';
     CommonModule,
     HeaderComponent,
     InformationComponent,
-    CardsComponent
+    CardsComponent,
+    FooterComponent
   ],
   templateUrl: './objective.component.html',
   styleUrls: ['./objective.component.scss']
